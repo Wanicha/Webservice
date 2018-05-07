15f83d77e1334a0720e29a5b0e1703f9c25cffe4
siripornbow <58160432@go.buu.ac.th>
2018-05-07 13:02:02 +0700

    Add files via upload

---
+++ add_comment.php
@@ -0,0 +1,56 @@
+<?php
+include "include/connectionDb.php";
+include "include/function.php";
+
+$strMode = $_POST["aMode"];
+$detail = rudeword($_REQUEST['aDetail']);
+$name = $_REQUEST['aName'];
+$strID = $_REQUEST["bID"];
+
+if ($strMode == "ADD")
+{
+    $sql = "INSERT INTO tbl_comment VALUES (0, '$name', '$detail', NOW(), '$strID')";
+    @mysql_query($sql) or die(mysql_error());
+}
+    $result = mysql_query("SELECT COUNT(*) AS numans FROM tbl_comment WHERE ref_blog_id='$strID' ORDER BY comment_id DESC");
+    $row = mysql_fetch_array($result);
+    $numans = $row['numans'];
+ ?>
+        <div style="padding-bottom:10px;" >
+         <span style="font-size:15px;color:#666;"><b>ร่วมแสดงความคิดเห็น(<?=$numans;?>)</b></span>
+        </div>
+ <?
+
+ $result = mysql_query("SELECT*, DATE_FORMAT(date_post, '%d-%m-%Y %H:%i:%s')
+ AS datepost FROM tbl_comment WHERE ref_blog_id='$strID' ORDER BY comment_id DESC");
+  while ($row = mysql_fetch_array($result)) {
+      $cid = $row['comment_id'];
+      $name = $row['name'];
+      $detail = nl2br(htmlspecialchars($row['detail']));
+      $date_post = $row['datepost'];
+ ?>
+        <div class="content">
+        <table width="90%" border="0" cellspacing="3" cellpadding="0">
+         <tr>
+            <td rowspan="2"><span class="answer"><b><?=$name;?></b></span>
+        <font style="color:#666">แสดงความคิดเห็น : <span class="date"><?=$date_post;?></span><br/><br/>
+        <?=$detail;?><font></td>
+         </tr>
+<?
+    $result = mysql_query("SELECT * FROM tbl_blog WHERE blog_id='$strID'");
+    $row = mysql_fetch_array($result);
+
+    if ($row['user_id'] == $_SESSION['userid']) {
+?>
+        <tr>
+            <td align="right"><img src="" border="0" width="20" height="20" onClick="doDelete('DELETE','<?=$strID;?>','<?=$cid;?>')"/></td>
+            </tr>
+<? } ?>
+        </table>
+        </div>
+        <div style="padding-bottom:10px;"></div>
+ <?
+  }
+
+  mysql_close($objConnect);
+  ?>
---
+++ del_comment.php
@@ -0,0 +1,60 @@
+<?php
+include "include/connectionDb.php";
+
+$bid = $_POST['bID'];
+$cid = $_POST['cID'];
+$mode = $_POST['aMode'];
+
+if ($mode =="DELETE") {
+    $result = mysql_query("SELECT * FROM tbl_blog WHERE blog_id='$bid'");
+    $row = mysql_fetch__array($result);
+
+    if ($row['user_id'] == $_SESSION['userid']) {
+       $sql = "DELETE FROM tbl_comment where comment_id= '$cid'";
+       $result = @mysql_query($sql) or die(mysql_error()); 
+    }
+}
+
+    $result = mysql_query("SELECT COUNT(*) AS numans FROM tbl_comment 
+    WHERE ref_blog_id='$bid' ORDER BY comment_id DESC");
+    $row = mysql_fetch_array($result);
+    $numans = $row['numans'];
+?>
+    <div style="padding-bottom:10px;">
+    <span style="font-size:15px;color:#666;"><b>ร่วมแสดงความคิดเห็น (<?=$numans;?>)</b></span>
+    </div>
+<?
+    $result = mysql_query("SELECT * FROM tbl_comment WHERE ref_blog_id='$bid' ORDER BY comment_id DESC");
+    while ($row = mysql_fetch_array($result)) {
+        $cid = $row['comment_id'];
+        $name = $row['name'];
+        $detail = nl2br(htmlspecialchars($row['detail']));
+        $date_post = $row['date_post'];
+        $time_post = $row['time_post'];
+?>
+        <div class="content">
+        <table width="90%" border="0" cellspacing="3" cellpadding="0">
+        
+        <tr>
+                <td rowspan="2"><span class="answer"><b><?=$name;?></b></span>
+                <font style="color:#666">แสดงความคิดเห็นเมื่อ : <span class="date"><?=$date_post;?> - <?=$time_post;?></span><br /><br /><?=$detail;?></font></td>
+        </tr>
+<?
+    $result = mysql_query("SELECT * FROM tbl_blog WHERE blog_id='$strID'");
+    $row = mysql_fetch_array($result);
+
+    if($row['user_id'] == $_SESSION['userid']) {
+?>
+        <tr>
+            <td align="right"><img src=""> border="0" widht="20" height="20" onClick="doDelete('DELETE','<?=$strID;?>','<?=$cid;?>')"/></td>
+
+    </tr>
+<? } ?>
+        </table>
+        </div>
+        <div style="padding-bottom:10px;"> </div>
+<?
+}
+
+mysql_close($objConnect);
+?>
